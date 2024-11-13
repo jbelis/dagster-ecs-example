524e0efe50bec4a2bf962be9ef8514fd29193ccc
Jacques Bélissent <jbelis@gmail.com>
2024-11-13 15:28:26 +0100

    remove out of process run

--- example_pipeline/executor.py
+++ example_pipeline/executor.py
@@ -1,8 +1,9 @@
-from dagster import execute_job, DagsterInstance, reconstructable
+from dagster import execute_job, DagsterInstance, reconstructable, ExecuteInProcessResult
 from example_pipeline import defs
 
 def return_job_def():
     return defs.get_job_def(name="example_job")
 
-def run():
-    execute_job(reconstructable(return_job_def), instance=DagsterInstance.get())
+def run() -> ExecuteInProcessResult:
+    #execute_job(reconstructable(return_job_def), instance=DagsterInstance.get())
+    return return_job_def().execute_in_process()
